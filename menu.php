a5ac0af26cb184a0196c060d155cfce061d355ef
Merit Paist <meripai@greeny.cs.tlu.ee>
2015-09-14 13:51:23 +0300

    parandused

--- menu.php
+++ menu.php
@@ -1,25 +1,22 @@
-<h3>Menüü</h3>
-<?php echo $page_file_name; ?>
+<h3>MenÃ¼Ã¼</h3>
 <ul>
 	<?php
-	//ükskõik mis lehe puhul näitab linki, home lehe puhul nime	
+	//Ã¼kskÃµik mis lehe puhul nÃ¤itab linki, home lehe puhul nime	
 		if($page_file_name != "home.php") { ?>
 			
-	      <li><a href="home.php>Avaleht</a></li>
+	      <li><a href="home.php">Avaleht</a></li>
 		<?php } else { ?>	
 		  <li>Avaleht</li>
 		<?php } ?>
 		
-	<li><a href="login.php"">Logi sisse</a></li>
-	
+		
 	<?php
-	//ükskõik mis lehe puhul näitab linki, home lehe puhul nime	
+	//Ã¼kskÃµik mis lehe puhul nÃ¤itab linki, home lehe puhul nime	
 		if($page_file_name != "login.php") { ?>
 			
-	      <li><a href="login.php>Logi sisse</a></li>
+	      <li><a href="login.php">Logi sisse</a></li>
 		<?php } else { ?>	
 		  <li>Logi sisse</li>
 		<?php } ?>
 		
-	<li><a href="login.php"">Logi sisse</a></li>
 </ul>
